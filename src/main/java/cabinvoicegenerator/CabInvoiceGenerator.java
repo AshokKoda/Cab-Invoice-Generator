e846ea1c5d7059b87b44f427607de3747ef80b1f
Ashok Koda <a.koda59@gmail.com>
2022-01-04 13:43:29 +0530

    Added: Welcome Message

---
+++ src/main/java/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -0,0 +1,11 @@
+package cabinvoicegenerator;
+
+public class CabInvoiceGenerator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("-------------Welcome to Cab Invoice Generator-------------");
+
+	}
+
+}
