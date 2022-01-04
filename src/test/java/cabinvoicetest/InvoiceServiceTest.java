a62a146e47fb7c1102baabe9aa5f0a5511ef1d6b
Ashok Koda <a.koda59@gmail.com>
2022-01-04 13:59:47 +0530

    Added: Functionality to culculate fare

---
+++ src/test/java/cabinvoicetest/InvoiceServiceTest.java
@@ -0,0 +1,16 @@
+package cabinvoicetest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import cabinvoicegenerator.CabInvoiceGenerator;
+
+public class InvoiceServiceTest {
+
+	@Test
+    public void givenDistanceAndTime_whenCalculateFare_ShouldReturnTotalFare() {
+		CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+        double totalFare = invoiceService.calculateFare(2.0, 5);
+        Assert.assertEquals(25.0, totalFare, 0.0);
+    }
+}
